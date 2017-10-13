db3bff18350728444cf09675de8e874b06961bc5
Toad <eric@falsquelle.com>
2017-10-13 15:03:05 +0200

    feat(Unity): Add position tree with GPS location

--- TreeMapgotchy/Assets/_Scripts/tree/TreeServiceScript.cs
+++ TreeMapgotchy/Assets/_Scripts/tree/TreeServiceScript.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Wrld;
-//using Wrld.Space;
+using Wrld;
+using Wrld.Space;
 
 
 public class TreeServiceScript : MonoBehaviour 
@@ -13,49 +13,106 @@ public class TreeServiceScript : MonoBehaviour
 			
 		}
 
-	// Update is called once per frame
-		void Update () {
-			
-		}
-//	static LatLong pointA = LatLong.FromDegrees(37.783372, -122.400834);
-//	static LatLong pointB = LatLong.FromDegrees(37.784560, -122.402092);
-//
-//	public GeographicTransform coordinateFrame;
-//	public Transform box;
+	static LatLong pointA = LatLong.FromDegrees(48.6887105, 2.2077153);
+	static LatLong pointB = LatLong.FromDegrees(48.6881722, 2.2088526);
+
+	public GeographicTransform coordinateFrame;
+	public Transform box;
+	public bool initiated = false;
 //
 //	// Use this for initialization
 //	void Start () {
 //		
 //	}
 //
-//	void OnEnable()
-//	{
-//		Api.Instance.GeographicApi.RegisterGeographicTransform(coordinateFrame);
-//		StartCoroutine(Example());
-//		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-//		cube.AddComponent<Rigidbody>();
-//		cube.transform.position = new Vector3(0.0f, 40.0f, 0);
-//		box = cube.transform;
-//	}
+	void OnEnable()
+	{
+		if (coordinateFrame != null) {
+			Debug.Log ("coordinateFrame is not null ");
+		}
+
+		if (Api.Instance != null) {
+			Debug.Log ("Instance is not null " + Api.Instance.ToString ());
+		} else {
+			Debug.Log ("Instance is null ");
+		}
+
+		if (Api.Instance.GeographicApi != null) {
+			Debug.Log ("GeographicApi is not null ");
+		}
+
+	}
 //	
 //	// Update is called once per frame
-//	void Update () {
-//		
-//	}
+	void Update () {
+		if (!initiated) {
+			if (Api.Instance != null) {
+				Debug.Log ("Initiate datas ");
+				Api.Instance.GeographicApi.RegisterGeographicTransform(coordinateFrame);
+				StartCoroutine(Example());
+//				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//				cube.AddComponent<Rigidbody>();
+//				cube.transform.position = new Vector3(0.0f, 40.0f, 0);
+//				box = cube.transform;
+			} else {
+				Debug.Log ("Instance is null ");
+			}
+			initiated = true;
+		}
+	}
 //
-//	IEnumerator Example()
+	IEnumerator Example()
+	{
+		Api.Instance.CameraApi.MoveTo(pointA, distanceFromInterest: 1000, headingDegrees: 0, tiltDegrees: 45);
+
+
+		while (true)
+		{
+			yield return new WaitForSeconds(2.0f);
+			calculateAltitude(pointA);
+			coordinateFrame.SetPosition(pointA);
+			yield return new WaitForSeconds(2.0f);
+			calculateAltitude(pointB);
+			coordinateFrame.SetPosition(pointB);
+		}
+	}
+
+	void calculateAltitude(LatLong latLong){
+		double altitude;
+		var success = Api.Instance.BuildingsApi.TryGetAltitudeAtLocation(latLong, out altitude);
+		if (success) {
+			Debug.Log ("success " + altitude);
+			//			var boxLocation = LatLong.FromDegrees (latLong.GetLatitude (), latLong.GetLongitude ());
+			//			var boxAnchor = Instantiate (boxPrefab) as GameObject;
+			//			boxAnchor.GetComponent<GeographicTransform> ().SetPosition (boxLocation);
+
+			// var box = boxAnchor.transform.GetChild (0);
+			// box.localPosition = new Vector3 (0.0f, , 0.0f);
+			// Destroy (boxAnchor, 2.0f);
+			box.localPosition = new Vector3(0.0f, (float)altitude, 0.0f);
+		} else {
+			// Debug.Log ("error ");
+			box.localPosition = new Vector3(0.0f, 150.0f, 0.0f);
+		}
+	}
+
+
+//	void MakeBox(LatLong latLong)
 //	{
-//		Api.Instance.CameraApi.MoveTo(pointA, distanceFromInterest: 1000, headingDegrees: 0, tiltDegrees: 45);
-//		box.localPosition = new Vector3(0.0f, 40.0f, 0.0f);
-//
-//		while (true)
+//		double altitude;
+//		var success = Api.Instance.BuildingsApi.TryGetAltitudeAtLocation(latLong, out altitude);
+//		if (success)
 //		{
-//			yield return new WaitForSeconds(2.0f);
-//			coordinateFrame.SetPosition(pointA);
-//			yield return new WaitForSeconds(2.0f);
-//			coordinateFrame.SetPosition(pointB);
+//			var boxLocation = LatLong.FromDegrees(latLong.GetLatitude(), latLong.GetLongitude());
+//			var boxAnchor = Instantiate(boxPrefab) as GameObject;
+//			boxAnchor.GetComponent<GeographicTransform>().SetPosition(boxLocation);
+//
+//			var box = boxAnchor.transform.GetChild(0);
+//			box.localPosition = new Vector3(0.0f, (float)altitude, 0.0f);
+//			Destroy(boxAnchor, 2.0f);
 //		}
 //	}
+
 //
 //	private void OnDisable()
 //	{

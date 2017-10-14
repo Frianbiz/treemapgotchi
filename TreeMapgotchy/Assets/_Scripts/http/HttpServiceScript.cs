44d8be73cd2394bbc07521809112fc886ff8d246
Toad <eric@falsquelle.com>
2017-10-14 21:35:33 +0200

    feat(Map): call the api to get Tree's datas and place them in the map

---
+++ TreeMapgotchy/Assets/_Scripts/http/HttpServiceScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HttpServiceScript : MonoBehaviour {
+
+	string api_url = "http://api.treemagotchi.frianbiz.com/api/users";
+
+	public static UserList list = null;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(getDatas());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public IEnumerator getDatas(){
+		Debug.Log("dans le store");
+
+		// Create a form object for sending high score data to the server
+		// WWWForm form = new WWWForm();
+
+		// Create a download object
+		WWW download = new WWW( api_url);
+
+		// Debug.Log("after create download");
+
+		// Wait until the download is done
+		yield return download;
+
+		Debug.Log("after return download");
+//		if (download.responseHeaders.Count > 0)
+//		{
+//			foreach (KeyValuePair<string, string> entry in download.responseHeaders)
+//			{
+//				Debug.Log(entry.Value + "=" + entry.Key);
+//			}
+//		}
+
+		if(!string.IsNullOrEmpty(download.error)) {
+			Debug.Log(download.error);
+			// print( "Error downloading: " + download.error );
+		} else {
+			Debug.Log("result" + download.text);
+			// string data = "{/""list":"+download.text+"}";
+			// string data = "[{"id":1,"name":"gro","latitude":"48.68338749999999","longitude":"2.2056852000000617","number_scan":8,"average_impact":"208.750000"}];
+			// Debug.Log("datas" + data);
+			// User user = JsonUtility.FromJson<User> (download.text);
+			list = JsonUtility.FromJson<UserList> (download.text);
+
+			// show the highscores
+			// Debug.Log("User " + list.ToString());
+//			Debug.Log("result" + list.ToString());
+
+//			User user = JsonUtility.FromJson<UserList> ("{list:"+download.text+"}");
+//			// show the highscores
+//			Debug.Log("result" + list.ToString());
+		}
+	}
+}
+
+[Serializable]
+public class User
+{
+	public int id;
+	public string name;
+	public float latitude;
+	public float longitude;
+	public int number_scan;
+	public float average_impact;
+}
+
+[Serializable]
+public class UserList
+{
+	public List<User> result;
+}
+
+//{
+//	"id":1,
+//	"name":"gro",
+//	"latitude":"48.68338749999999",
+//	"longitude":"2.2056852000000617",
+//	"number_scan":8,
+//	"average_impact":"208.750000"
+//}
--- TreeMapgotchy/Assets/_Scripts/tree/TreeServiceScript.cs
+++ TreeMapgotchy/Assets/_Scripts/tree/TreeServiceScript.cs
@@ -16,40 +16,52 @@ public class TreeServiceScript : MonoBehaviour
 	static LatLong pointA = LatLong.FromDegrees(48.6887105, 2.2077153);
 	static LatLong pointB = LatLong.FromDegrees(48.6881722, 2.2088526);
 
-	public GeographicTransform coordinateFrame;
-	public Transform box;
+//	public GeographicTransform coordinateFrame;
+//	public Transform box;
 	public bool initiated = false;
+
+	public GameObject treeBad;
+	public GameObject treeMiddle;
+	public GameObject treeGood;
+
 //
 //	// Use this for initialization
 //	void Start () {
 //		
 //	}
 //
-	void OnEnable()
-	{
-		if (coordinateFrame != null) {
-			Debug.Log ("coordinateFrame is not null ");
-		}
-
-		if (Api.Instance != null) {
-			Debug.Log ("Instance is not null " + Api.Instance.ToString ());
-		} else {
-			Debug.Log ("Instance is null ");
-		}
-
-		if (Api.Instance.GeographicApi != null) {
-			Debug.Log ("GeographicApi is not null ");
-		}
-
-	}
+//	void OnEnable()
+//	{
+//		if (coordinateFrame != null) {
+//			Debug.Log ("coordinateFrame is not null ");
+//		}
+//
+//		if (Api.Instance != null) {
+//			Debug.Log ("Instance is not null " + Api.Instance.ToString ());
+//		} else {
+//			Debug.Log ("Instance is null ");
+//		}
+//
+//		if (Api.Instance.GeographicApi != null) {
+//			Debug.Log ("GeographicApi is not null ");
+//		}
+//
+//	}
 //	
 //	// Update is called once per frame
 	void Update () {
-		if (!initiated) {
+		if (!initiated && HttpServiceScript.list != null) {
 			if (Api.Instance != null) {
 				Debug.Log ("Initiate datas ");
-				Api.Instance.GeographicApi.RegisterGeographicTransform(coordinateFrame);
-				StartCoroutine(Example());
+				// Api.Instance.GeographicApi.RegisterGeographicTransform(coordinateFrame);
+				Debug.Log ("userList " + HttpServiceScript.list.result.Count);
+				foreach (User currentUser in HttpServiceScript.list.result)
+				{
+					Debug.Log ("generateTree " + currentUser.name + " " + currentUser.id  + " " + currentUser.average_impact + " " + currentUser.latitude + " " + currentUser.longitude);
+					generateTree (currentUser);
+					// System.Console.WriteLine(element);
+				}
+				// StartCoroutine(Example());
 //				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 //				cube.AddComponent<Rigidbody>();
 //				cube.transform.position = new Vector3(0.0f, 40.0f, 0);
@@ -61,40 +73,67 @@ public class TreeServiceScript : MonoBehaviour
 		}
 	}
 //
-	IEnumerator Example()
-	{
-		Api.Instance.CameraApi.MoveTo(pointA, distanceFromInterest: 1000, headingDegrees: 0, tiltDegrees: 45);
+//	IEnumerator Example()
+//	{
+//		Api.Instance.CameraApi.MoveTo(pointA, distanceFromInterest: 1000, headingDegrees: 0, tiltDegrees: 45);
+//
+//		while (true)
+//		{
+//			yield return new WaitForSeconds(2.0f);
+//			calculateAltitude(pointA);
+//			coordinateFrame.SetPosition(pointA);
+//			yield return new WaitForSeconds(2.0f);
+//			calculateAltitude(pointB);
+//			coordinateFrame.SetPosition(pointB);
+//		}
+//	}
 
+	void generateTree(User user){
+		var boxLocation = LatLong.FromDegrees (user.latitude, user.longitude);
 
-		while (true)
-		{
-			yield return new WaitForSeconds(2.0f);
-			calculateAltitude(pointA);
-			coordinateFrame.SetPosition(pointA);
-			yield return new WaitForSeconds(2.0f);
-			calculateAltitude(pointB);
-			coordinateFrame.SetPosition(pointB);
-		}
-	}
-
-	void calculateAltitude(LatLong latLong){
-		double altitude;
-		var success = Api.Instance.BuildingsApi.TryGetAltitudeAtLocation(latLong, out altitude);
-		if (success) {
-			Debug.Log ("success " + altitude);
-			//			var boxLocation = LatLong.FromDegrees (latLong.GetLatitude (), latLong.GetLongitude ());
-			//			var boxAnchor = Instantiate (boxPrefab) as GameObject;
-			//			boxAnchor.GetComponent<GeographicTransform> ().SetPosition (boxLocation);
-
-			// var box = boxAnchor.transform.GetChild (0);
-			// box.localPosition = new Vector3 (0.0f, , 0.0f);
-			// Destroy (boxAnchor, 2.0f);
-			box.localPosition = new Vector3(0.0f, (float)altitude, 0.0f);
+		GameObject box = new GameObject ();
+		box.AddComponent<GeographicTransform> ();
+		GameObject representation;
+		if (user.average_impact <= 170f) {
+			representation= Instantiate (treeGood) as GameObject;	
+		} else if (user.average_impact >= 480f) {
+			representation= Instantiate (treeBad) as GameObject;	
 		} else {
-			// Debug.Log ("error ");
-			box.localPosition = new Vector3(0.0f, 150.0f, 0.0f);
+			representation= Instantiate (treeMiddle) as GameObject;	
 		}
+		representation.transform.SetParent(box.transform);
+		// box.transform.p.addChild(representation);
+		// boxAnchor.AddComponent<GeographicTransform> ();
+		Api.Instance.GeographicApi.RegisterGeographicTransform(box.GetComponent<GeographicTransform>());
+		box.GetComponent<GeographicTransform>().SetPosition(boxLocation);
+//		var box = boxAnchor.transform.GetChild (0);
+//		// box.localPosition = new Vector3 (0.0f, , 0.0f);
+//		// Destroy (boxAnchor, 2.0f);
+//		 var box = boxAnchor.transform;
+		representation.transform.localPosition = new Vector3 (0.0f, 150.0f, 0.0f);
+		representation.transform.localScale = new Vector3 (10.0f, 10.0f, 10.0f);
+
 	}
+
+
+//	void calculateAltitude(LatLong latLong){
+//		double altitude;
+//		var success = Api.Instance.BuildingsApi.TryGetAltitudeAtLocation(latLong, out altitude);
+//		if (success) {
+//			Debug.Log ("success " + altitude);
+//			//			var boxLocation = LatLong.FromDegrees (latLong.GetLatitude (), latLong.GetLongitude ());
+//			//			var boxAnchor = Instantiate (boxPrefab) as GameObject;
+//			//			boxAnchor.GetComponent<GeographicTransform> ().SetPosition (boxLocation);
+//
+//			// var box = boxAnchor.transform.GetChild (0);
+//			// box.localPosition = new Vector3 (0.0f, , 0.0f);
+//			// Destroy (boxAnchor, 2.0f);
+//			box.localPosition = new Vector3(0.0f, (float)altitude, 0.0f);
+//		} else {
+//			// Debug.Log ("error ");
+//			box.localPosition = new Vector3(0.0f, 150.0f, 0.0f);
+//		}
+//	}
 
 
 //	void MakeBox(LatLong latLong)

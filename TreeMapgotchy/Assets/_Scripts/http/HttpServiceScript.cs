ca86dfc9da000fe8d0e0aa79c6a064de14d1cd34
Toad <eric@falsquelle.com>
2017-10-17 13:44:38 +0200

    chore(Code): Clean Code

--- TreeMapgotchy/Assets/_Scripts/http/HttpServiceScript.cs
+++ TreeMapgotchy/Assets/_Scripts/http/HttpServiceScript.cs
@@ -13,56 +13,36 @@ public class HttpServiceScript : MonoBehaviour {
 	void Start () {
 		StartCoroutine(getDatas());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	public IEnumerator getDatas(){
 		Debug.Log("dans le store");
 
-		// Create a form object for sending high score data to the server
-		// WWWForm form = new WWWForm();
-
 		// Create a download object
 		WWW download = new WWW( api_url);
-
-		// Debug.Log("after create download");
 
 		// Wait until the download is done
 		yield return download;
 
 		Debug.Log("after return download");
-//		if (download.responseHeaders.Count > 0)
-//		{
-//			foreach (KeyValuePair<string, string> entry in download.responseHeaders)
-//			{
-//				Debug.Log(entry.Value + "=" + entry.Key);
-//			}
-//		}
 
 		if(!string.IsNullOrEmpty(download.error)) {
 			Debug.Log(download.error);
-			// print( "Error downloading: " + download.error );
 		} else {
 			Debug.Log("result" + download.text);
-			// string data = "{/""list":"+download.text+"}";
-			// string data = "[{"id":1,"name":"gro","latitude":"48.68338749999999","longitude":"2.2056852000000617","number_scan":8,"average_impact":"208.750000"}];
-			// Debug.Log("datas" + data);
-			// User user = JsonUtility.FromJson<User> (download.text);
 			list = JsonUtility.FromJson<UserList> (download.text);
-
-			// show the highscores
-			// Debug.Log("User " + list.ToString());
-//			Debug.Log("result" + list.ToString());
-
-//			User user = JsonUtility.FromJson<UserList> ("{list:"+download.text+"}");
-//			// show the highscores
-//			Debug.Log("result" + list.ToString());
 		}
 	}
 }
+	
+// sample
+//{
+//	"id":1,
+//	"name":"gro",
+//	"latitude":"48.68338749999999",
+//	"longitude":"2.2056852000000617",
+//	"number_scan":8,
+//	"average_impact":"208.750000"
+//}
 
 [Serializable]
 public class User
@@ -80,12 +60,3 @@ public class UserList
 {
 	public List<User> result;
 }
-
-//{
-//	"id":1,
-//	"name":"gro",
-//	"latitude":"48.68338749999999",
-//	"longitude":"2.2056852000000617",
-//	"number_scan":8,
-//	"average_impact":"208.750000"
-//}
